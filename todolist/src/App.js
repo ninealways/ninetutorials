e4ed81aa687f27dfef4c2abd8c968a8653f505e0
navnit <navnit_dav@yahoo.co.in>
2022-06-15 21:05:50 +0530

    todo add, delete functions added

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,10 +1,40 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputData, setInputData] = useState('');
+  const [list, setList] = useState([]);
+
+  const handleInputChange = (event) => {
+    setInputData(event.target.value)
+  }
+
+  const handleAddClick = () => {
+    setList([...list, inputData]);
+    setInputData('');
+  }
+
+  const deleteListItem = (index) => {
+    const newList = list.filter((item, ind) => {
+      return ind !== index
+    });
+
+    setList(newList)
+  }
+
   return (
-    <div className="App">
-      To Do List
-      <p>Child of App</p>
+    <div className="todo-list">
+      <input type="text" value={inputData} onChange={(event) => handleInputChange(event)} />
+      <button className="go-btn" onClick={handleAddClick}>ADD</button>
+      <ul className="my-list">
+        {list?.map((item, index) => {
+          return <li key={index}>
+            {item}
+            <button className="edit">Edit</button>
+            <button className="delete" onClick={() => deleteListItem(index)}>Delete</button>
+            </li>
+        })}
+      </ul>
     </div>
   );
 }
